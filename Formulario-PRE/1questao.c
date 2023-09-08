ea3701336d058a191fceb682b4c8e94da1e5ed08
Thierry-DV <thierryuchoa7@gmail.com>
2023-09-08 09:09:19 -0300

    update

--- Formulario-PRE/1questao.c
+++ Formulario-PRE/1questao.c
@@ -2,35 +2,35 @@
 #include <math.h>
 
 int main() {
-    float numero;
+    float numero, quadrado, cubo, raizQuadrada, raizCubica, resto;
     
-    printf("Digite um número positivo e maior que zero: ");
+    printf("Digite um numero positivo e maior que zero: ");
     scanf("%f", &numero);
     
     if (numero <= 0) {
-        printf("O número deve ser positivo e maior que zero.\n");
+        printf("O numero deve ser positivo e maior que zero.\n");
         return 1;
     }
     
     // Calcula e mostra o número ao quadrado
-    float quadrado = numero * numero;
-    printf("O número ao quadrado é: %.2f\n", quadrado);
+    quadrado = numero * numero;
+    printf("O numero ao quadrado e: %.2f\n", quadrado);
     
     // Calcula e mostra o número ao cubo
-    float cubo = numero * numero * numero;
-    printf("O número ao cubo é: %.2f\n", cubo);
+    cubo = numero * numero * numero;
+    printf("O numero ao cubo e: %.2f\n", cubo);
     
     // Calcula e mostra a raiz quadrada do número
-    float raizQuadrada = sqrt(numero);
-    printf("A raiz quadrada do número é: %.2f\n", raizQuadrada);
+    raizQuadrada = sqrt(numero);
+    printf("A raiz quadrada do numero e: %.2f\n", raizQuadrada);
     
     // Calcula e mostra a raiz cúbica do número
-    float raizCubica = cbrt(numero);
-    printf("A raiz cúbica do número é: %.2f\n", raizCubica);
+    raizCubica = cbrt(numero);
+    printf("A raiz cubica do numero e: %.2f\n", raizCubica);
     
     // Calcula e mostra o resto da divisão do número por 3
-    float resto = fmod(numero, 3);
-    printf("O resto da divisão por 3 é: %.2f\n", resto);
+    resto = fmod(numero, 3);
+    printf("O resto da divisao por 3 e: %.2f\n", resto);
     
     return 0;
 }
