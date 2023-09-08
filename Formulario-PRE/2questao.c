364de62387e83ebae8ec53b89f9f4b4bdba95088
Thierry-DV <thierryuchoa7@gmail.com>
2023-09-08 09:31:42 -0300

    Update

---
+++ Formulario-PRE/2questao.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+float hora;
+int horas, minutos,minutostot,minutostotais, segundos, segundoshoras, segundosminutos;
+
+int main() {
+
+    printf("Digite a hora (no formato Horas.Minutos): ");
+    scanf("%f", &hora);
+
+    // Extrai a parte inteira (horas) e a parte decimal (minutos)
+    horas = (int)hora;
+    minutos = (int)((hora - horas) * 100); // Multiplicamos por 100 para converter a parte decimal em minutos
+
+    //Calcula os minutos
+    minutostot = horas * 60;
+    minutostotais = minutostot + minutos;
+    
+    // Calcula os segundos
+    segundosminutos = (minutos * 60);
+    segundoshoras = (horas * 3600);
+    segundos = segundoshoras + segundosminutos;
+
+    if (horas >= 0 && horas <= 23 && minutos >= 0 && minutos <= 59) {
+        printf("Horas: %d\n", horas);
+        printf("Minutos: %d\n", minutostotais);
+        printf("Segundos: %d\n", segundos);
+    } else {
+        printf("Hora invalida.\n");
+    }
+    return 0;
+}
