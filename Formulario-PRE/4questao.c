a03a0dcea8e6768e0e78016c7bec4f1fde01ea0e
Thierry-DV <thierryuchoa7@gmail.com>
2023-09-08 09:58:00 -0300

    up 4

--- Formulario-PRE/4questao.c
+++ Formulario-PRE/4questao.c
@@ -8,11 +8,11 @@ int main() {
     unsigned long long fatorial;
 
     do {
-        printf("Digite um número inteiro positivo (ou 0 para sair): ");
+        printf("Digite um numero inteiro positivo (ou 0 para sair): ");
         scanf("%d", &numero);
 
         if (numero < 0) {
-            printf("Por favor, digite um número inteiro positivo.\n");
+            printf("Por favor, digite um numero inteiro positivo.\n");
         } else if (numero > 0) {
             fatorial = 1;
 
@@ -21,7 +21,7 @@ int main() {
                 fatorial *= i;
             }
 
-            printf("Número: %d\n", numero);
+            printf("Numero: %d\n", numero);
             printf("Fatorial: %llu\n", fatorial);
         }
     } while (numero != 0);
